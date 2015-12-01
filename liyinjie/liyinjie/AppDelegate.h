2aa0ec8f0ea7264721db3cb27080acaf83c46b2e
CNM <lyj9106141225@126.com>
2015-12-01 10:16:33 +0800

    fred3

---
+++ liyinjie/liyinjie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  liyinjie
+//
+//  Created by tarena17 on 15/12/1.
+//  Copyright © 2015年 tarena17. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
